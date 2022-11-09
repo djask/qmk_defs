0693ae60ca1121c4f033d43b7e48da2373eb1496
djask <>
2022-11-09 09:17:25 +0000

    Fix the numpad shift layer keys

--- untitledkb1/keymaps/midsplit/keymap.c
+++ untitledkb1/keymaps/midsplit/keymap.c
@@ -27,11 +27,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_NO ,  KC_CAPS, KC_LGUI, KC_LALT, MO(1),           KC_MHEN , KC_HENK ,KC_SPC,           KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 [1] = LAYOUT(
-  _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , _______, KC_INS , KC_DEL ,_______,KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, _______,  
-  _______, KC_F5  , KC_F6  , KC_F7  , KC_F8  , _______, KC_HOME, KC_PGUP,_______,KC_P7  , KC_P8  , KC_P9  , KC_PPLS, _______,  
-  _______, KC_F9  , KC_F10 , KC_F11 , KC_F12 , _______, KC_END , KC_PGDN,_______,KC_P4  , KC_P5  , KC_P6  , _______, _______,  
-  _______, _______, _______, _______, _______, _______, _______, _______,_______,KC_P1  , KC_P2  , KC_P3  , KC_PENT, _______,  
-  _______, _______, _______, _______, _______,          QK_BOOT, NK_TOGG,KC_P0,           KC_PDOT, _______, _______, _______
+  _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , _______, _______, _______,_______,KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, _______,  
+  _______, KC_F5  , KC_F6  , KC_F7  , KC_F8  , _______, KC_INS , KC_DEL ,_______,KC_P7  , KC_P8  , KC_P9  , KC_PPLS, _______,  
+  _______, KC_F9  , KC_F10 , KC_F11 , KC_F12 , _______, KC_HOME, KC_PGUP,_______,KC_P4  , KC_P5  , KC_P6  , _______, _______,  
+  _______, _______, _______, _______, _______, _______, KC_END , KC_PGDN,_______,KC_P1  , KC_P2  , KC_P3  , KC_PENT, _______,  
+  _______, _______, _______, _______, _______,          QK_BOOT, NK_TOGG,KC_P0,           _______, KC_PDOT, _______, _______
 )
 };
 
