9bf6663a213e7378f012e1b91eab730f13821a5d
djask <>
2022-12-02 07:08:54 +1100

    Change manufacturer and simpler default keymap for 2882

--- n860_2882/n860_2882.c
+++ n860_2882/n860_2882.c
@@ -5,5 +5,5 @@ void keyboard_post_init_user(void) {
   // debug_enable=true;
   // debug_matrix=true;
   // debug_keyboard=true;
-  //debug_mouse=true;
+  // debug_mouse=true;
 }
--- n860_2882/rules.mk
+++ n860_2882/rules.mk
@@ -1,7 +1,4 @@
-# This file intentionally left blank
 CUSTOM_MATRIX=yes
-MOUSEKEYS_ENABLE=yes
-
 NO_USB_STARTUP_CHECK = yes
 
 SRC += matrix.c
