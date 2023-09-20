82615328c58bbbe3e1cd1bab1788983d7b48ffef
djask <djask@users.noreply.github.com>
2023-09-21 07:00:43 +1000

    Format keymap files and update Autoshift alias to newest

--- untitledkb1/keymaps/midsplit/keymap.c
+++ untitledkb1/keymaps/midsplit/keymap.c
@@ -8,13 +8,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_INS , KC_DEL ,KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
   KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_HOME, KC_PGUP,KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_END , KC_PGDN,KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-  KC_NO ,  KC_CAPS, KC_LGUI, KC_LALT, MO(1),           MO(2)  , MO(2)   ,KC_SPC,           KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+  KC_NO ,  KC_CAPS, KC_LGUI, KC_LALT, MO(1),           MO(2)  , MO(2)  ,KC_SPC,           KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 [1] = LAYOUT(
-  KC_GRV , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_WH_U, QK_BOOT, KC_ACL0,_______,KC_BTN2, _______, KC_MINS, KC_EQL , _______, 
-  _______, KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_WH_D, _______, KC_ACL1,KC_WH_U,KC_BTN1, KC_MS_U, KC_LBRC, KC_RBRC, _______, 
+  KC_GRV , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_WH_U, QK_BOOT, KC_ACL0,_______,KC_BTN2, _______, KC_MINS, KC_EQL , _______,
+  _______, KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_WH_D, _______, KC_ACL1,KC_WH_U,KC_BTN1, KC_MS_U, KC_LBRC, KC_RBRC, _______,
   _______, KC_F9  , KC_F10 , KC_F11 , KC_F12 , KC_WH_L, _______, KC_ACL2,KC_WH_D,KC_MS_L, KC_MS_D, KC_MS_R, _______, KC_QUOT,
-  _______, _______, _______, _______, _______, _______, _______, KC_WH_L,KC_WH_R,_______, _______, _______, _______, _______, 
+  _______, _______, _______, _______, _______, _______, _______, KC_WH_L,KC_WH_R,_______, _______, _______, _______, _______,
   _______, _______, _______, _______, _______,          AS_TOGG, NK_TOGG,KC_MINS,         _______, _______, _______, _______
 ),
 [2] = LAYOUT(
