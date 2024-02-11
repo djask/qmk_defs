c39b9bc56b589eb444dbb2ee490e0976864a2301
djask <djask@users.noreply.github.com>
2024-02-11 11:11:36 +1100

    move shift to a normal position for default layout

--- sofle_choc/keymaps/default/keymap.c
+++ sofle_choc/keymaps/default/keymap.c
@@ -20,15 +20,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] = LAYOUT(
     KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,     KC_7,     KC_8,    KC_9,    KC_0,    KC_BSPC,
     KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,     KC_U,     KC_I,    KC_O,    KC_P,    KC_BSLS,
-    KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                       KC_H,     KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_GRV,   KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    QK_BOOT,  QK_BOOT, KC_N,     KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-                      MO(2),   KC_LGUI, KC_LALT, KC_LSFT, MO(1),    MO(2),   KC_RSFT,  KC_SPC,   MO(1),   KC_RGUI
+    KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                       KC_H,     KC_J,     KC_K,    KC_L,    KC_SCLN, KC_ENT,
+    KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    QK_BOOT,  QK_BOOT, KC_N,     KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                      KC_LGUI, KC_SPC,  KC_LALT, MO(1)  , MO(2),    MO(2),   MO(1),    KC_SPC,   KC_UNDS, KC_RGUI
 ),
-
 [1] = LAYOUT(
-    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______,                    KC_PSCR, KC_SCRL, KC_BRK,  KC_MINS, KC_EQL,  _______,
+    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______,                    KC_PSCR, KC_SCRL, KC_BRK,  _______, KC_MINS, KC_EQL ,
     _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______,                    KC_INS,  KC_HOME, KC_PGUP, KC_LBRC, KC_RBRC, _______,
-    KC_CAPS, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,                    KC_DEL,  KC_END,  KC_PGDN, KC_LPRN, KC_RPRN, _______,
+    KC_CAPS, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,                    KC_DEL,  KC_END,  KC_PGDN, KC_LPRN, KC_RPRN, KC_QUOT,
     _______, _______, _______, _______, _______, _______, NK_TOGG,  _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
                       _______, _______, _______, _______, _______,  _______, _______, KC_MINS, _______, _______
 ),
@@ -39,7 +38,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______, _______, _______,  _______, _______, KC_P1,   KC_P2,   KC_P3,   KC_PENT, _______,
                       _______, _______, _______, _______, _______,  _______, _______, KC_P0,   _______, KC_PDOT
 ),
-
 };
 
 
